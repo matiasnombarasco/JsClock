a96df128ce1a7dc65c0518f8d091b337b9a78c84
Rafa <raf_ws@hotmail.com>
2017-04-15 15:37:21 -0300

    Creacion y lectura de cookies

--- js/clock.js
+++ js/clock.js
@@ -2,11 +2,23 @@
  * Created by Tox on 15/04/2017.
  */
 
-var horaInicio = new Date().setHours(9,0,0);
-var horaFin = new Date().setHours(12,0,0);
+//# region - valores por defecto -
+var horaI = 8;
+var minI = 0;
+var horaF = 17;
+var minF = 0;
+//#endregion
+
+var inputHoraI;
+var inputMinI;
+var inputHoraF;
+var inputMinF;
+
+var horaInicio = new Date().setHours(horaI,minI,0);
+var horaFin = new Date().setHours(horaF,minF,0);
 var porcentaje;
 var horalocal;
-var intervalo = 60000;
+var intervalo;
 
 $(document).ready(function () {
     ControlarHorasInicioFin();
@@ -44,4 +56,30 @@ function ControlarHorasInicioFin() {
     if(horaInicio>=horaFin){
         horaFin += 86400000;
     }
+}
+
+function CrearCookie(hora1, min1, hora2, min2){
+    var d = new Date().getTime() + (5*365*24*60*60*1000); //fecha de expiración: luego de 5 años
+    var fechaExpiracion = new Date (d) ;
+    document.cookie = "horaI=" + hora1 + ";"
+                    + "minI=" + min1 + ";"
+                    + "horaF=" + hora2 + ";"
+                    + "minF=" + min2 + ";"
+                    + "expires=" + fechaExpiracion;
+}
+
+function LeerCookies() {
+    var cook = document.cookie;
+    if (cook == null){
+        CrearCookie(horaI,minI,horaF,minF);
+    } else{
+        horaI = cook.horaI;
+        minI = cook.minI;
+        horaF = cook.horaF;
+        minF = cook.minF;
+    }
+}
+
+function ActualizarCookies() {
+    CrearCookie(inputHoraI,inputMinI,inputHoraF,inputMinF);
 }
